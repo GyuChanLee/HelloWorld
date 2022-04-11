08ce0ecdfa58bf46e2ee634dee66bd4fa2952d95
admin <admin@YD2-18>
2022-04-11 14:22:47 +0900

    반복문2

---
+++ HelloWorld/src/co/edu/variable/IfExample2.java
@@ -0,0 +1,24 @@
+package co.edu.variable;
+
+public class IfExample2 
+{
+
+	public static void main(String[] args) 
+	{
+		int num1, num2;
+		
+		num1 = 111;
+		num2 = 222;
+		
+		if(num1 > num2)
+		{
+			System.out.println("큰수는 : "+ num1);
+		}
+		else
+		{
+			System.out.println("큰수는 : "+ num2);
+		}
+	}
+
+}
+// if-else문
